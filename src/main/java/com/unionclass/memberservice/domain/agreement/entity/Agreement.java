c840173755ead06177e58f67343d214d45db3e1e
Ahn-donghwan <ddh1713flrwls1713@gmail.com>
2025-06-05 14:32:59 +0900

    feat: 약관동의 엔티티 제약 조건 추가

--- src/main/java/com/unionclass/memberservice/domain/agreement/entity/Agreement.java
+++ src/main/java/com/unionclass/memberservice/domain/agreement/entity/Agreement.java
@@ -2,29 +2,44 @@ package com.unionclass.memberservice.domain.agreement.entity;
 
 import com.unionclass.memberservice.common.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Agreement extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Comment("이용약관 UUID")
+    @Column(nullable = false, unique = true, length = 36)
     private Long uuid;
+
+    @Comment("이용약관 명")
+    @Column(nullable = false)
     private String name;
 
     @Lob
+    @Comment("이용약관 내용")
     private String content;
+
+    @Comment("필수 여부")
+    @Column(nullable = false)
     private Boolean required;
 
+    @Comment("삭제 여부")
+    @Column(nullable = false)
     private Boolean deleted;
+
+    @Comment("삭제 일시")
     private LocalDateTime deletedAt;
 
     @Builder
